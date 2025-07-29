92099c5b435e7eb3f1938bdf62b91e35e75bb5bd
Jorge Navarro Garcia <jorge.navarro.garcia@nttdata.com>
2025-07-29 13:14:17 +0200

    Crea un pipe personalizado para controlar los estados

---
+++ frontend/src/app/pipes/car-condition-pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'carCondition'
+})
+export class CarConditionPipe implements PipeTransform {
+
+  /*transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }*/
+
+  transform(value: string): string {
+    let tag = "";
+
+    if(value == "0") {
+      tag = "Nuevo";
+    } else if(value < "100" ) {
+      tag = "Km 0";
+    }else {
+      tag = "Ocasión";
+    }
+    
+    return `${tag}`;
+  }
+
+}
