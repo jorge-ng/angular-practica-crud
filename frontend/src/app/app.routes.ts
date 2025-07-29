1925a4356b015c41fe95be79d6f12262c3d0358c
Jorge Navarro Garcia <jorge.navarro.garcia@nttdata.com>
2025-07-29 13:10:07 +0200

    Configura ruta dinámica para leer el id desde la ruta cars/:id

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home-component/home-component';
+import { CarDetailsComponent } from './components/car-details-component/car-details-component';
 
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'cars/:id', component: CarDetailsComponent }
 ];
